ab7a9f8ed8baab86280347502d5026d10c81e0a6
eddiezhang <career.eddie.zhang@gmail.com>
2021-01-13 15:12:00 +1100

    Rectified docker tag latest

--- find_latest.sh
+++ find_latest.sh
@@ -13,7 +13,7 @@ fi
 
 if [[ ${status} == *${output}* ]]; then
    docker build --build-arg version=${output} -t ${image}:${output} .
-   docker tag {image}:${output} {image}:latest
+   docker tag ${image}:${output} ${image}:latest
    docker push ${image}:${output}
    docker push ${image}:latest
 fi
